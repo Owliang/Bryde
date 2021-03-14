4bc64c4d8f7c5682e752d68b236b2954122af448
Owliang <owliang1234@gmail.com>
2021-03-14 14:53:00 +0700

    delete index.js, index.css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,13 @@
-import React , { Component,useState  } from 'react';
+import React , { Component, useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
-import theme from './theme';
+import reportWebVitals from './reportWebVitals';
+import App from './App';
+
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
 
-/*ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);*/
-
-//test api
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
