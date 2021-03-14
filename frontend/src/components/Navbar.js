41ba091fda6df9b3e6b475818b045d130c55d3a9
Owliang <owliang1234@gmail.com>
2021-03-14 14:43:52 +0700

    delete yarn, unused file (please use npm)

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react"
+import { makeStyles, AppBar, Toolbar, Typography, Button, IconButton, Box, Drawer, List, ListItem, ListItemText } from '@material-ui/core'
+import MenuIcon from '@material-ui/icons/Menu'
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      color: '#FFFFFF'
+    },
+    title: {
+      flexGrow: 1,
+    },
+    drawer: {
+      width: '240px',
+      flexShrink: 0,
+    },
+    appBar: {
+      background: '#333333',
+      zIndex: theme.zIndex.drawer + 1,
+    },
+    list: {
+      width: 240,
+    },
+}));
+
+const mobile = { xs: 'block', sm: 'none' }
+const desktop = { xs: 'none', sm: 'block' }
+  
+export default function Navbar() {
+
+    const classes = useStyles();
+    const [isDrawerOpen, setDrawerOpen] = useState(false)
+
+    const toggleDrawer = (state) => () => {
+        setDrawerOpen(state)
+    }
+
+    const drawerItem = () => (
+        <div
+            className={classes.list}
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+        >
+            <List>
+                {['Home', 'Course', 'Livestream', 'OffMessage', 'Account', 'Help Center'].map((text) => (
+                    <ListItem button key={text}>
+                        <ListItemText primary={text} />
+                    </ListItem>
+                ))}
+            </List>
+        </div>
+    );
+
+    return (
+        <div className={classes.root}>
+            <AppBar className={classes.appBar}>
+                <Toolbar className={classes.toolBar}>
+                    <Box display={mobile}>
+                        <IconButton className={classes.menuButton} onClick={toggleDrawer(true)} edge="start">
+                            <MenuIcon />
+                        </IconButton>
+                    </Box>
+                    <Typography variant="h6" color="secondary">OffDemand</Typography>
+                    <Box display={desktop}>
+                      <Button color="secondary">Home</Button>
+                      <Button color="secondary">Course</Button>
+                      <Button color="secondary">Livestream</Button>
+                      <Button color="secondary">OffMessage</Button>
+                      <Button color="secondary">Account</Button>
+                      <Button color="secondary">Help Canter</Button>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+            <Drawer className={classes.drawer} onClose={toggleDrawer(false)} open={isDrawerOpen} anchor="left">
+              {drawerItem()}
+            </Drawer>
+            <Toolbar />
+        </div>
+    );
+}
