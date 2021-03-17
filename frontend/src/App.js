05c0cd37210ea35aa7d4e7ebc7f754da405695d4
Owliang <owliang1234@gmail.com>
2021-03-17 11:45:36 +0700

    add auth & redirect system

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,52 @@
 import React, { Component, createContext, useState, useContext, useEffect, useCallback } from "react"
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom"
 import { Container, Box } from '@material-ui/core'
 import CourseList from './components/course/CourseList'
 import Navbar from './components/Navbar'
 import Landing from './components/Landing'
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <Box bgcolor="background.dark">
-        <Container maxWidth="lg">
-          <Box bgcolor="background.dark" height='100vh'> 
-            <Router>
-              <Switch>
-                <Route path="/courses">
-                  <CourseList/>
-                </Route>
-                <Route path="/">
-                  <Landing />
-                </Route>
-              </Switch>
-            </Router>
-          </Box>
-        </Container>
-      </Box>
-    </div>
-  );
+
+    const auth = localStorage.getItem('auth') == 'true' ? true : false
+
+    // use this instead of Route
+    // if not login (auth is false), PrivateRoute will redirect to Landing page
+    const PrivateRoute = ({ component: Component, ...rest }) => (
+        <Route
+            render={(props) => (
+                auth ? <Component {...props} /> : <Redirect to='/' />
+            )}
+            {...rest}
+        />
+    )
+
+    const LoginRoute = ({ component: Component, ...rest }) => (
+        <Route
+            render={(props) => (
+                auth ? <Redirect to='/courses' /> : <Component {...props} />
+            )}
+            {...rest}
+        />
+    )
+
+    return (
+        <div>
+            {auth && <Navbar />}
+            <Box bgcolor="background.dark">
+                <Container maxWidth="lg">
+                    <Box bgcolor="background.dark" height='100vh'> 
+                        <Router>
+                            <Switch>
+                                <PrivateRoute path='/courses' component={CourseList} />
+                                <LoginRoute path="/" component={Landing} />
+                                <Route path='/courses' component={CourseList} />
+                            </Switch>
+                        </Router>
+                    </Box>
+                </Container>
+            </Box>
+        </div>
+    );
 }
 
 export default App;
