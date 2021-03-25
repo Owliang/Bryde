41d5634ab69377e345204f5b7d7981f6eb9971a7
Job <chayapoi-p@hotmail.com>
2021-03-25 11:17:41 +0700

    Fix Backend Cors

---
+++ backend/routes/create_course.js
@@ -0,0 +1,64 @@
+var mongo = require('mongodb');
+var express = require('express');
+var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://127.0.0.1:27017/";
+const { body, validationResult, check } = require('express-validator');
+const { UnavailableForLegalReasons } = require('http-errors');
+var fs = require('fs');
+var multer  = require('multer')
+var upload = multer({ 
+    storage:multer.diskStorage({
+        destination: function (req, file, cb) {
+            // ใช้งาน path module กำหนดโฟลเดอร์ที่จะบันทึกไฟล์
+            cb(null, 'uploads/')
+        },
+        filename: function (req, file, cb) {
+            // เปลี่ยนชื่อไฟล์ ในที่นี้ใช้เวลา timestamp ต่อด้วยชือ่ไฟล์เดิม
+            // เช่นไฟล์เดิมเป็น bird.png ก็จะได้เป็น  1558631524415-bird.png
+            cb(null, file.originalname)
+        }
+    })
+})
+/* GET home page. */
+
+router.post('/', function(req, res, next) {
+    res.send('responce');
+
+router.get('/', function(req, res, next) {
+    res.render('upload');
+    //res.json({result:'Response from create course page'})
+});
+course_upload = upload.fields([{name:'attach_photo',maxcount:1},{name:'attach_video',maxcount:15}]);
+router.post('/',course_upload,function(req, res, next) {
+        MongoClient.connect(url, function(err, db) {
+            if (err) {
+              res.json({result:false,error:err})
+            }
+            else{
+                var dbo = db.db("BrydeTech");
+                var video = [];
+                for(i=0;i<(req.files['attach_video']).length;i++){
+                    video.push(fs.readFileSync('uploads/'+req.files['attach_video'][i].originalname));
+                }
+                var myobj = {name:req.body.name,
+                    tutor:req.body.tutor,
+                    price:req.body.price,
+                    subject:req.body.subject,
+                    description:req.body.description,
+                    link:req.body.link,
+                  photo_buffer:fs.readFileSync('uploads/'+req.files['attach_photo'][0].originalname),
+                  video_buffer:video,
+                    rating:7,
+                    enrolled_date:'-',
+                    student:[]};
+                dbo.collection("courses").insertOne(myobj,function(err,response){
+                    if (err) throw err;
+                    db.close();
+                });
+                res.json({result:true,error:""})
+            }
+        });
+});
+
+module.exports = router;
---
+++ backend/routes/question_more.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var mongo = require('mongodb');
+var router = express.Router();
+var mongoose = require('mongoose');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://127.0.0.1:27017/";
+var Binary = require('mongodb').Binary;
+var fs = require('fs');
+var data = fs.readFileSync('aaa.txt');
+console.log(data);
+/* GET home page. */
+
+router.get('/', function(req, res, next) {
+        //res.json({result:req.query.subject});
+        MongoClient.connect(url, function(err, db) {
+            if (err) {
+                throw err;
+            }
+            else{
+                var dbo = db.db("BrydeTech");
+                dbo.collection("Q&A").find({topic:req.query.topic}, { projection: { _id: 0,} }).toArray(function(err, result) {
+                    if (err) throw err
+                    res.json({result:result});
+                    db.close();
+                })
+            }
+        });
+});
+router.post('/', function(req, res, next) {
+    MongoClient.connect(url, function(err, db) {
+        if (err) {
+            throw err;
+        }
+        else{
+            var dbo = db.db("BrydeTech");
+            var myquery = { topic: req.body.topic };
+            var newvalues = { $push: {comment:req.body.comment} };
+            dbo.collection("Q&A").updateOne(myquery, newvalues, function(err, res) {
+                if (err) throw err;
+                db.close();
+            });
+        }
+    });
+    MongoClient.connect(url, function(err, db) {
+        if (err) {
+            throw err;
+        }
+        else{
+            var dbo = db.db("BrydeTech");
+            var myquery = { topic: req.body.topic };
+            var newvalues = { $push: {comment:req.body.comment} };
+            dbo.collection("Q&A").find({"topic":req.body.topic}, { projection: { _id: 0, follower:1} }).toArray(function(err, result) {
+                if (err) {
+                    throw err;
+                }
+                res.json({ result : true , result:result,error : ""});
+                db.close();
+            });
+        }
+    });
+});
+module.exports = router;
