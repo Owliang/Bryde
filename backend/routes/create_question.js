9355360cf732984a8a8c0078cae92afc0c7ba555
tawann <korawichtawan@gmail.com>
2021-04-07 18:35:24 +0700

    fix creator to username in create_question

--- backend/routes/create_question.js
+++ backend/routes/create_question.js
@@ -43,7 +43,7 @@ router.post('/', function (req, res, next) {
                     var dbo = db.db("BrydeTech");
                     var myobj = {
                         topic: fields.topic,
-                        creator: fields.creator,
+                        creator: fields.username,
                         subject: fields.subject,
                         description: fields.content,
                         attach_photo: files['attatch_photo'].name,
