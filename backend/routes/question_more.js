6c69882a8fac0f1a7b25c85bb0d5e96b3dc8cb1c
tawann <korawichtawan@gmail.com>
2021-04-08 20:04:28 +0700

    case sensitive,noti,suggestion q&a

--- backend/routes/question_more.js
+++ backend/routes/question_more.js
@@ -11,7 +11,6 @@ var fs = require('fs');
 /* GET home page. */
 
 router.get('/', function(req, res, next) {
-        //res.json({result:req.query.subject});
         MongoClient.connect(url, function(err, db) {
             if (err) {
                 res.json({result:false,error:err})
@@ -41,31 +40,15 @@ check("id","Please enter id").not().isEmpty(),check("comment","Please enter comm
             var dbo = db.db("BrydeTech");
             var id = new mongo.ObjectID(req.body.id)
             var myquery = { _id:id};
-            //var newvalues = {$push:{comment:{writer:req.body.username,desc:req.body.comment}}}
             var newvalues = { $push: {writer:req.body.username,comment:req.body.comment} };
-            dbo.collection("Q&A").updateOne(myquery, newvalues, function(err, res) {
+            dbo.collection("Q&A").findOneAndUpdate(myquery, newvalues, function(err, result) {
                 if (err){
                     res.json({result:false,error:err})
                 }
+                res.json({result:true,error:"",id:result.value._id,topic:result.value.topic,latest_comment:req.body.comment,follower:result.value.follower})
                 db.close();
             });
-            res.json({result:true,error:""})
         }
     });
-    /*MongoClient.connect(url, function(err, db) {
-        if (err) {
-            res.json({result:false,error:err})
-        }
-        else{
-            var dbo = db.db("BrydeTech");
-            dbo.collection("Q&A").find({_id:id}, { projection: { _id: 0, follower:1} }).toArray(function(err, result) {
-                if (err) {
-                    res.json({result:false , error:err})
-                }
-                res.json({ result : true , result:result,error : ""});
-                db.close();
-            });
-        }
-    });*/
 });
 module.exports = router;
